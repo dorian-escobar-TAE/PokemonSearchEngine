0d515b5dbf97e1cd90152ebe4f2e300de90517c2
Eric Martinez Acelas <eamartinez.acelas@outlook.com>
2020-09-28 17:56:41 -0500

    eliminando componentes de instalación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  public title: String;
+  public year: number;
+
+  constructor(){
+    this.title = "ISE - engineering group";
+    this.year = 2020
+  }
 }
