f8f1a7deaa2bf88f02266a87cdc37afb9e0e932b
Dorian Escobar <dorianescobar@rccl.com>
2020-09-29 19:09:01 -0500

    Carga de componente header

---
+++ src/app/componente-header/componente-header.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ComponenteHeaderComponent } from './componente-header.component';
+
+describe('ComponenteHeaderComponent', () => {
+  let component: ComponenteHeaderComponent;
+  let fixture: ComponentFixture<ComponenteHeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ComponenteHeaderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ComponenteHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/componente-header/componente-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-header',
+  templateUrl: './componente-header.component.html',
+  styleUrls: ['./componente-header.component.css']
+})
+export class ComponenteHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
