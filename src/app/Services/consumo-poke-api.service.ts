76b5f32b12f226718ed8feb993829df4abad6b17
isenv <isenv.projects@outlook.com>
2020-10-12 16:10:35 -0500

    consumiendo api reproduciendo tantos card pokemon retorne la api

--- src/app/Services/consumo-poke-api.service.ts
+++ src/app/Services/consumo-poke-api.service.ts
@@ -6,4 +6,20 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 })
 export class ConsumoPokeAPIService {
 
+  url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=150'
+
+  constructor(
+    private _http: HttpClient) {
+    console.log('inyectando servicio')
+  }
+
+  getConsumo() {
+    let header = new HttpHeaders()
+      .set('Type-content', 'aplication/json')
+
+    return this._http.get(this.url, {
+      headers: header
+    })
+  }
+
 }
